4da324c5d678dd152a2d960a6153deecea19acce
Paarth Kukreja <paarthkukreja@outlook.com>
2024-07-19 21:15:48 +0400

    chore: update next.js dependency to version 14.2.5

--- pages/api/tag.js
+++ pages/api/tag.js
@@ -1,24 +1,49 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import OpenAI from "openai";
+import OpenAI, { toFile } from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAIAPI,
 });
 
 import fs from "fs";
+const { Duplex } = require("stream"); // Native Node Module
+
+function bufferToStream(myBuffer) {
+  let tmp = new Duplex();
+  tmp.push(myBuffer);
+  tmp.push(null);
+  return tmp;
+}
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: "100mb",
+    },
+  },
+};
+const { Readable } = require("stream");
 
 export default async function handler(req, res) {
+  const reqBodyData = await JSON.parse(req.body);
+  // // console.log(await bufferToStream(await Buffer(reqBodyData.video.data)));
+  // // const stream = typeof reqBodyData.video;
+  // console.log(typeof reqBodyData.video);
+  // console.log(reqBodyData.video);
+  // console.log(await Buffer(await reqBodyData.video));
   async function main() {
+    const video = await fetch(reqBodyData.video);
+
     const transcription = await openai.audio.transcriptions.create({
-      file: fs.createReadStream("public/audio-trim.mp3"),
+      file: await toFile(video),
       model: "whisper-1",
       response_format: "srt",
       timestamp_granularities: ["segment"],
     });
-
-    res.status(200).json({ transcriptionJson: transcription });
+    console.log(transcription);
+    res.status(200).send(transcription);
   }
 
+  console.log("hi");
   main();
   // if (response.status !== 200) {
   //   res.status(response.status).json(response);
